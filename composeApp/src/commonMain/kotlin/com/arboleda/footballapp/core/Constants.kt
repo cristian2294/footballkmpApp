7f5c1388747da2bbf6cbfc288086dfc582a9e6d6
cristian <doko.stan@gmail.com>
2025-05-15 23:09:32 -0500

    Refactor: Configure HttpClient with base URL and API key
    Configured `HttpClient` in `dataModule` to use the base URL and include the API key in the request headers by using `DefaultRequest` plugin.
    Created `Constants.kt` file to store the base URL and API key header name.

---
+++ composeApp/src/commonMain/kotlin/com/arboleda/footballapp/core/Constants.kt
@@ -0,0 +1,6 @@
+package com.arboleda.footballapp.core
+
+object Constants {
+    const val BASE_URL = "api-football-v1.p.rapidapi.com/v3/"
+    const val NAME_HEADER_API_KEY = "x-apisports-key"
+}
--- composeApp/src/commonMain/kotlin/com/arboleda/footballapp/di/dataModule.kt
+++ composeApp/src/commonMain/kotlin/com/arboleda/footballapp/di/dataModule.kt
@@ -1,10 +1,14 @@
 package com.arboleda.footballapp.di
 
+import com.arboleda.footballapp.core.ApiKeyProvider
+import com.arboleda.footballapp.core.Constants.BASE_URL
+import com.arboleda.footballapp.core.Constants.NAME_HEADER_API_KEY
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.http.ContentType
 import io.ktor.http.URLProtocol
+import io.ktor.http.headers
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import org.koin.dsl.module
@@ -19,8 +23,10 @@ val dataModule =
                 install(DefaultRequest) {
                     url {
                         protocol = URLProtocol.HTTPS
-                        host = "rickandmortyapi.com"
-                        // parameters.append("api_key", "123456789")
+                        host = BASE_URL
+                    }
+                    headers {
+                        append(NAME_HEADER_API_KEY, ApiKeyProvider.getApiKey())
                     }
                 }
             }
